10dcf7b614b41af9980a4061f207de0744492cfd
mcometa <menfcometa@gmail.com>
2021-08-23 03:26:21 +0000

    Deploying to gh-pages from @ mcometa/todo-app@e8f9ff3968208bcc25e1d79fe60d200ae9cae52b 🚀

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const devConfig = require('./webpack.dev')
-const prodConfig = require('./webpack.production')
-
-module.exports = () => {
-  // merge default configuration with a chosen mode configuration
-  return process.env.NODE_ENV === 'development' ? devConfig : prodConfig
-}
